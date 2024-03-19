5381784365feb97119c45cd9c7b3daa46e7c81ab
Mohamed Arsath <90707804+arsath-13@users.noreply.github.com>
2024-03-19 20:14:14 +0530

    SSA 001 : Battery Models Created with its properties.

--- application/models/battery.py
+++ application/models/battery.py
@@ -1,2 +1,29 @@
 class Battery:
-    pass
+    def __init__(self, itemId, itemName, segment):
+        self._itemId = itemId
+        self._itemName = itemName
+        self._segment = segment
+
+    @property
+    def itemId(self):
+        return self._itemId
+
+    @itemId.setter
+    def itemId(self, value):
+        self._itemId = value
+
+    @property
+    def itemName(self):
+        return self._itemName
+
+    @itemName.setter
+    def itemName(self, value):
+        self._itemName = value
+
+    @property
+    def segment(self):
+        return self._segment
+
+    @segment.setter
+    def segment(self, value):
+        self._segment = value
